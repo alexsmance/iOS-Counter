06950c54800562280843ba40114ec927d9b63625
Alex Mance <alexmance@hentamine.com>
2020-07-30 20:42:21 +1000

    Initial Commit

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -9,13 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  @State var count = 0
+  
+  var body: some View {
+    VStack() {
+      Spacer()
+      HStack {
+        Spacer()
+        Button("-", action: DecreaseCount)
+        Spacer()
+        Text("\(count)")
+        .font(.title)
+        Spacer()
+        Button("+", action: IncreaseCount)
+        Spacer()
+      }
+      Spacer()
+      HStack {
+        Button("Reset", action: ResetCount)
+      }
     }
+  }
+  
+  func IncreaseCount() {
+    count += 1
+  }
+  
+  func DecreaseCount() {
+    if count > 0 {
+      count = count - 1
+    }
+  }
+  
+  func ResetCount() {
+    count = 0
+  }  
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+      Group {
         ContentView()
+        .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+        .previewDisplayName("iPhone 11 Pro Max")
+        
+        ContentView()
+        .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (2nd generation)"))
+        .previewDisplayName("iPad Pro")
+      }
     }
 }
