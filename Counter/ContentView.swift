a5697b3f26f9dd2543d4d79b78db93e8b7c0d5c9
Alex Mance <alexmance@hentamine.com>
2020-07-30 18:21:48 +1000

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Alex Mance on 30/7/20.
+//  Copyright © 2020 Alex Mance. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
